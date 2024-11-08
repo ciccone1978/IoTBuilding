97bab33855dcaf6451812c70e170c10dd4823719
ciccone1978 <ciccone1978@gmail.com>
2024-11-08 11:13:49 +0100

    ESP32 BLINK LED

--- ESP32_LED_BLINK/src/main.cpp
+++ ESP32_LED_BLINK/src/main.cpp
@@ -1,17 +1,29 @@
 #include <Arduino.h>
 
-#define LED_PIN 2
+#define LED_PIN1 2
+#define LED_PIN2 4
+#define LED_PIN3 16
 
 void setup() {
   Serial.begin(115200);
-  pinMode(LED_PIN, OUTPUT);
+  pinMode(LED_PIN1, OUTPUT);
+  pinMode(LED_PIN2, OUTPUT);
+  pinMode(LED_PIN3, OUTPUT);
+
+  digitalWrite(LED_PIN1, LOW);
+  digitalWrite(LED_PIN2, LOW);
+  digitalWrite(LED_PIN3, LOW);
 }
 
 void loop() {
-  Serial.println("ON");
-  digitalWrite(LED_PIN, HIGH);
+  
+  digitalWrite(LED_PIN1, HIGH);
+  digitalWrite(LED_PIN2, HIGH);
+  digitalWrite(LED_PIN3, HIGH);
   delay(1000);
-  Serial.println("OFF");
-  digitalWrite(LED_PIN, LOW);
+  digitalWrite(LED_PIN1, LOW);
+  digitalWrite(LED_PIN2, LOW);
+  digitalWrite(LED_PIN3, LOW);
   delay(1000);
+
 }
